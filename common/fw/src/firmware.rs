d8f45b8e12d604eae025004ae69f10badb867687
Mariotaku <mariotaku.lee@gmail.com>
2024-05-29 11:14:05 +0900

    fixing local installation on Windows

--- common/fw/src/firmware.rs
+++ common/fw/src/firmware.rs
@@ -111,7 +111,7 @@ impl Firmware {
         return if cfg!(feature = "linux-install") {
             PathBuf::from("/usr/share/webosbrew/compat-checker/data")
         } else {
-            PathBuf::from("common").join("data")
+            PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("..").join("data").canonicalize().unwrap()
         };
     }
 }
